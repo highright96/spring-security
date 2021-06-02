5f9e1c791fc51eaebad5bce6de20004d18efc07c
highright96 <nsw5296@naver.com>
2021-06-02 18:13:14 +0900

    [Add] basic-login

---
+++ server/src/main/java/dev/highright96/server/config/CustomAuthDetails.java
@@ -0,0 +1,20 @@
+package dev.highright96.server.config;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Component
+public class CustomAuthDetails implements AuthenticationDetailsSource<HttpServletRequest, RequestInfo> {
+
+    @Override
+    public RequestInfo buildDetails(HttpServletRequest request) {
+        return RequestInfo.builder()
+                .remoteIp(request.getRemoteAddr())
+                .sessionId(request.getSession().getId())
+                .loginTime(LocalDateTime.now())
+                .build();
+    }
+}
---
+++ server/src/main/java/dev/highright96/server/controller/SecurityMessage.java
@@ -0,0 +1,16 @@
+package dev.highright96.server.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SecurityMessage {
+    private Authentication auth;
+    private String message;
+}
